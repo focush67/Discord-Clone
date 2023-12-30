0b5c58acad6d904ef2b90fe1af682b6042e45a0f
Sparsh Verma <sparshv70@gmail.com>
2023-12-30 14:28:57 +0530

    Setting themes for UI

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,12 @@
+import { ModeToggle } from "@/components/toggle-mode"
 import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return(
-    <div className='font-bold flex flex-col'>
+    <div className='font-bold flex justify-between'>
       <UserButton  
         afterSignOutUrl="/"
-        
       />
+      <ModeToggle />
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Merriweather_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Merriweather_Sans({ subsets: ["latin"] });
 
@@ -17,8 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
